7d5dcb51bf1aec8c99d86412c010eafa4274ece4
Yate Fulham <yate@cryptape.com>
2018-08-30 16:52:10 +0900

    Creating TabbedButtonsView

--- Neuron/Sections/Common/TabbedButtonsView.swift
+++ Neuron/Sections/Common/TabbedButtonsView.swift
@@ -2,20 +2,11 @@
 //  TabbedButtonsView.swift
 //  Neuron
 //
-//  Created by James Chen on 2018/08/30.
+//  Created by Yate Fulham on 2018/08/30.
 //  Copyright © 2018 Cryptape. All rights reserved.
 //
 
 import UIKit
 
 class TabbedButtonsView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
