3f034fbc95257da7d5683bcbf7d7e2855e8f13a6
Yate Fulham <yate@cryptape.com>
2018-08-30 14:36:42 +0900

    Layout wallet navbar buttons and connect actions

---
+++ Neuron/Sections/Wallet/RequestPaymentViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RequestPaymentViewController.swift
+//  Neuron
+//
+//  Created by Yate Fulham on 2018/08/30.
+//  Copyright © 2018 Cryptape. All rights reserved.
+//
+
+import UIKit
+
+class RequestPaymentViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
