f35bbb2fa9239642924c1991a1f62660f6bff65e
LuFP <lfp@cryptape.com>
2018-08-30 17:33:44 +0800

    import keystore

--- Neuron/Sections/Wallet/View/KeystoreViewController.swift
+++ Neuron/Sections/Wallet/View/KeystoreViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class KeystoreViewController: UITableViewController {
-    @IBOutlet weak var name: UITextField!
-    @IBOutlet weak var password: UITextField!
+class KeystoreViewController: UITableViewController, ImportTextViewCellDelegate, ImportWalletViewModelDelegate, QRCodeControllerDelegate {
     @IBOutlet weak var importButton: UIButton!
+    @IBOutlet weak var textViewCell: ImportTextViewCell!
+    var name: String? = ""
+    var password: String? = ""
+    var keystore: String? = ""
+    let viewModel = ImportWalletViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        textViewCell.delegate = self
+        viewModel.delegate = self
     }
+
+    @IBAction func nameChanged(_ sender: UITextField) {
+        name = sender.text
+        jugeImportButtonEnabled()
+    }
+
+    @IBAction func passwordChanged(_ sender: UITextField) {
+        password = sender.text
+        jugeImportButtonEnabled()
+    }
+
+    func didGetTextViewText(text: String) {
+        keystore = text
+        jugeImportButtonEnabled()
+    }
+
+    func didBackQRCodeMessage(codeResult: String) {
+        keystore = codeResult
+        jugeImportButtonEnabled()
+    }
+
+    func jugeImportButtonEnabled() {
+        if name?.count != 0 && password?.count != 0 && keystore?.count != 0 {
+            importButton.backgroundColor = ColorFromString(hex: "#456CFF")
+            importButton.isEnabled = true
+        } else {
+            importButton.backgroundColor = ColorFromString(hex: "#E9EBF0")
+            importButton.isEnabled = false
+        }
+    }
+
+    func didClickQRBtn() {
+        let qrCtrl = QRCodeController()
+        qrCtrl.delegate = self
+        self.navigationController?.pushViewController(qrCtrl, animated: true)
+    }
+
     @IBAction func importWallet(_ sender: UIButton) {
+        viewModel.importKeyStoreWallet(keyStore: keystore!, password: password!, name: name!)
+    }
+
+    func didPopToRootView() {
+        navigationController?.popToRootViewController(animated: true)
     }
 }
