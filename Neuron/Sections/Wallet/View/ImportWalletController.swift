ed98b31ab3d23f6518b26d1ee545c7b26bfda2fa
LuFP <lfp@cryptape.com>
2018-08-31 13:05:15 +0800

    Remove ImportPageViewController

--- Neuron/Sections/Wallet/View/ImportWalletController.swift
+++ Neuron/Sections/Wallet/View/ImportWalletController.swift
@@ -10,66 +10,68 @@
 import UIKit
 import RSKPlaceholderTextView
 
-enum SelectButtonStates {
-    case keystoreState
-    case mnemonicState
-    case privateKeyState
-}
-
 class ImportWalletController: UIViewController {
-
-    @IBOutlet weak var keystore: UIButton!
-    @IBOutlet weak var mnemonic: UIButton!
-    @IBOutlet weak var privatekey: UIButton!
-    @IBOutlet weak var slider: UIView!
-
-    var currentStates: SelectButtonStates! {
+    @IBOutlet weak var tabbedButtonView: TabbedButtonsView!
+    private var keystoreViewController: UIViewController!
+    private var mnemonicViewController: UIViewController!
+    private var privatekeyViewController: UIViewController!
+    private var importWalletPageViewController: UIPageViewController!
+    var pageViewControllers = [UIViewController]()
+    var currentIndex = 0 {
         didSet {
-            topButtonAndSliderPositionChanged(states: currentStates)
+            tabbedButtonView.selectedIndex = currentIndex
         }
     }
-    var pageViewController: ImportWalletPageViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "导入钱包"
-        currentStates = .keystoreState
-    }
-    @IBAction func changeToKestore(_ sender: UIButton) {
-        currentStates = .keystoreState
-    }
-    @IBAction func changeToMnemonic(_ sender: UIButton) {
-        currentStates = .mnemonicState
-    }
-    @IBAction func changeToPrivatekey(_ sender: UIButton) {
-        currentStates = .privateKeyState
-    }
-    func topButtonAndSliderPositionChanged(states: SelectButtonStates) {
-        keystore.setTitleColor(ColorFromString(hex: "#666666"), for: .normal)
-        mnemonic.setTitleColor(ColorFromString(hex: "#666666"), for: .normal)
-        privatekey.setTitleColor(ColorFromString(hex: "#666666"), for: .normal)
-        switch states {
-        case .keystoreState:
-            keystore.setTitleColor(themeColor, for: .normal)
-            slider.center.x = keystore.center.x
-//            pageViewController.pages.forEach { (importViewController) in
-                pageViewController.setViewControllers([pageViewController.pages[0]], direction: .reverse, animated: true, completion: nil)
-//            }
-        case .mnemonicState:
-            mnemonic.setTitleColor(themeColor, for: .normal)
-            slider.center.x = mnemonic.center.x
-            pageViewController.setViewControllers([pageViewController.pages[1]], direction: .reverse, animated: true, completion: nil)
-        case .privateKeyState:
-            privatekey.setTitleColor(themeColor, for: .normal)
-            slider.center.x = privatekey.center.x
-            pageViewController.setViewControllers([pageViewController.pages[2]], direction: .forward, animated: true, completion: nil)
-        }
+        tabbedButtonView.delegate = self
+        tabbedButtonView.buttonTitles = ["keyStore", "助记词", "私钥"]
+
+        importWalletPageViewController.delegate = self
+        importWalletPageViewController.dataSource = self
+        keystoreViewController = storyboard!.instantiateViewController(withIdentifier: "keystoreViewController")
+        mnemonicViewController = storyboard!.instantiateViewController(withIdentifier: "mnemonicViewController")
+        privatekeyViewController = storyboard!.instantiateViewController(withIdentifier: "privatekeyViewController")
+        pageViewControllers.append(keystoreViewController)
+        pageViewControllers.append(mnemonicViewController)
+        pageViewControllers.append(privatekeyViewController)
+        importWalletPageViewController.setViewControllers([keystoreViewController], direction: .forward, animated: false)
+
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "importWalletPageViewController" {
-            pageViewController = segue.destination as? ImportWalletPageViewController
+            importWalletPageViewController = segue.destination as? UIPageViewController
+        }
+    }
+}
+
+extension ImportWalletController: TabbedButtonsViewDelegate, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        print(viewController.description + "before")
+        currentIndex = pageViewControllers.index(of: viewController)!
+        let previousIndex = abs((currentIndex - 1) % pageViewControllers.count)
+        if currentIndex == 0 {
+            return nil
+        } else {
+            return pageViewControllers[previousIndex]
         }
     }
 
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        print(viewController.description + "after")
+        currentIndex = pageViewControllers.index(of: viewController)!
+        let nextIndex = abs((currentIndex + 1) % pageViewControllers.count)
+        if currentIndex == pageViewControllers.count - 1 {
+            return nil
+        } else {
+            return pageViewControllers[nextIndex]
+        }
+    }
+
+    func tabbedButtonsView(_ view: TabbedButtonsView, didSelectButtonAt index: Int) {
+        importWalletPageViewController.setViewControllers([pageViewControllers[index]], direction: .forward, animated: false, completion: nil)
+    }
 }
