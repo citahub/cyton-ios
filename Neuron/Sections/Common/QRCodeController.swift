761d292f2f1ae8a4ccd0abbd7448149a4d9c5614
cezres <cezr@sina.com>
2018-10-24 15:20:09 +0800

    AVCaptureDevice authorization

--- Neuron/Sections/Common/QRCodeController.swift
+++ Neuron/Sections/Common/QRCodeController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 protocol QRCodeControllerDelegate: class {
     func didBackQRCodeMessage(codeResult: String)
@@ -29,7 +30,23 @@ class QRCodeController: UIViewController, HRQRCodeScanToolDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "扫描二维码"
+        view.backgroundColor = UIColor.black
         share.delegate  = self
-        share.beginScanInView(view: view)
+        AVCaptureDevice.requestAccess(for: AVMediaType.video) { [weak self](result) in
+            guard let self = self else { return }
+            if result {
+                self.share.beginScanInView(view: self.view)
+            } else {
+                let alert = UIAlertController(title: "", message: "扫描二维码需要相机访问权限", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "开启", style: .default, handler: { (_) in
+                    let url = URL(string: UIApplication.openSettingsURLString)!
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }))
+                alert.addAction(UIAlertAction(title: "取消", style: .default, handler: { (_) in
+                    self.navigationController?.popViewController(animated: true)
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
 }
