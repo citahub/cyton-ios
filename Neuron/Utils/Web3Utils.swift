bdc9803fc0bd1c222476e85ca49d2e819e80d3c9
James Chen <james@ashchan.com>
2018-10-29 16:23:50 +0900

    Check in the missing Web3Utils.swift file

---
+++ Neuron/Utils/Web3Utils.swift
@@ -0,0 +1,12 @@
+//
+//  Web3Utils.swift
+//  Neuron
+//
+//  Created by James Chen on 2018/10/29.
+//  Copyright © 2018 Cryptape. All rights reserved.
+//
+
+import Foundation
+import web3swift
+
+typealias Web3Utils = web3swift.Web3Utils
