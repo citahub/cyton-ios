6adf75e355cc6c1db7fc214d00fb4becf4b55e4f
James Chen <james@ashchan.com>
2018-11-01 16:46:23 +0900

    Remove realm encryption key migration
    Now that keystore layer is totally rewritten and backward compatibility
    it doesn't make any sense to keep this.

--- Neuron/Utils/RealmHelper.swift
+++ Neuron/Utils/RealmHelper.swift
@@ -18,55 +18,11 @@ class RealmHelper {
         config.schemaVersion = schemaVersion
         config.migrationBlock = migrationBlock
 
-        if schemaVersion >= 2, let encryptionKey = getEncryptionKey() {
-            removeEncrptionFromRealm(key: encryptionKey)
-            deleteEncryptionKeyFromKeychain()
-        }
-
         Realm.Configuration.defaultConfiguration = config
     }
 }
 
 private extension RealmHelper {
-    static var keychainQuery: CFDictionary {
-        let keychainIdentifier = "org.nervos.Neuron"
-        return [
-            kSecClass: kSecClassKey,
-            kSecAttrApplicationTag: keychainIdentifier.data(using: .utf8, allowLossyConversion: false) as AnyObject,
-            kSecAttrKeySizeInBits: 512 as AnyObject,
-            kSecReturnData: true as AnyObject
-        ]  as CFDictionary
-    }
-
-    static func getEncryptionKey() -> Data? {
-        var dataTypeRef: AnyObject?
-        let status = withUnsafeMutablePointer(to: &dataTypeRef) { SecItemCopyMatching(keychainQuery, UnsafeMutablePointer($0)) }
-        if status == errSecSuccess {
-            return dataTypeRef as? Data
-        }
-
-        return nil
-    }
-
-    static func deleteEncryptionKeyFromKeychain() {
-        SecItemDelete(keychainQuery)
-    }
-
-    static func removeEncrptionFromRealm(key: Data) {
-        autoreleasepool {
-            var config = Realm.Configuration()
-            config.schemaVersion = schemaVersion
-            config.migrationBlock = migrationBlock
-            config.encryptionKey = key
-
-            let tempUrl = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).appendingPathComponent("temp.realm")
-            let realm = try! Realm(configuration: config)
-            try! realm.writeCopy(toFile: tempUrl, encryptionKey: nil)
-            try! FileManager.default.removeItem(at: config.fileURL!)
-            try! FileManager.default.moveItem(at: tempUrl, to: config.fileURL!)
-        }
-    }
-
     static var migrationBlock: MigrationBlock {
         return { migration, oldSchemaVersion in
             if oldSchemaVersion < schemaVersion {
