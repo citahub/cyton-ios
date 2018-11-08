200bdb0b8de961d107c321fce29e17a72f2bcf66
LuFP <lfp@cryptape.com>
2018-11-08 14:57:33 +0800

    User PasswordValidator

--- Neuron/Sections/Transaction/TransactionConfirmViewController.swift
+++ Neuron/Sections/Transaction/TransactionConfirmViewController.swift
@@ -181,8 +181,8 @@ class TransactionConfirmSendViewController: UIViewController {
 
     @IBAction func confirm(_ sender: Any) {
         let password = passwordTextField.text ?? ""
-        if password.lengthOfBytes(using: .utf8) == 0 {
-            Toast.showToast(text: "请输入密码")
+        if case .invalid(let reason) = PasswordValidator.validate(password: password) {
+            Toast.showToast(text: reason)
             return
         }
         delegate?.confirmWalletPassword(password: password)
