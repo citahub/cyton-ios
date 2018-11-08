3a9d6081af5fdee906e0258a70262b4acab6ac7c
LuFP <lfp@cryptape.com>
2018-11-08 15:09:02 +0800

    Fix gasSymbol

--- Neuron/Model/TokenModel.swift
+++ Neuron/Model/TokenModel.swift
@@ -60,6 +60,15 @@ class TokenModel: Object, Decodable {
             return .erc20
         }
     }
+    var gasSymbol: String {
+        switch type {
+        case .erc20, .ethereum:
+            return "ETH"
+        case .nervos, .nervosErc20:
+            return self.symbol
+        }
+    }
+
 
     enum CodingKeys: String, CodingKey {
         case name
