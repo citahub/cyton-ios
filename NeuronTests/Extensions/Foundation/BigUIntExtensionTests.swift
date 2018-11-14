7a1d02b870bc71dae8534deaa25529749b5585c4
James Chen <james@ashchan.com>
2018-11-14 10:05:29 +0900

    Add unit conversion extension

---
+++ NeuronTests/Extensions/Foundation/BigUIntExtensionTests.swift
@@ -0,0 +1,94 @@
+//
+//  BigUIntExtensionTests.swift
+//  NeuronTests
+//
+//  Created by James Chen on 2018/11/14.
+//  Copyright © 2018 Cryptape. All rights reserved.
+//
+
+import XCTest
+import BigInt
+@testable import Neuron
+
+class BigUIntExtensionTests: XCTestCase {
+    func testToWei() {
+        XCTAssertEqual(
+            BigUInt(1),
+            BigUInt(1).toWei(from: .wei)
+        )
+
+        XCTAssertEqual(
+            BigUInt("1000000000"),
+            BigUInt(1).toWei(from: .gwei)
+        )
+
+        XCTAssertEqual(
+            BigUInt("1000000000000000000"),
+            BigUInt(1).toWei(from: .ether)
+        )
+    }
+
+    func testToGwei() {
+        XCTAssertEqual(
+            BigUInt(1),
+            BigUInt("1000000000")!.toGwei(from: .wei)
+        )
+        XCTAssertEqual(
+            BigUInt(0),
+            BigUInt("10000")!.toGwei(from: .wei)
+        )
+
+        XCTAssertEqual(
+            BigUInt(1),
+            BigUInt(1).toGwei(from: .gwei)
+        )
+
+        XCTAssertEqual(
+            BigUInt("1000000000"),
+            BigUInt(1).toGwei(from: .ether)
+        )
+    }
+
+    func testToEther() {
+        XCTAssertEqual(
+            BigUInt(1),
+            BigUInt("1000000000000000000")!.toEther(from: .wei)
+        )
+        XCTAssertEqual(
+            BigUInt(0),
+            BigUInt("10000")!.toEther(from: .wei)
+        )
+
+        XCTAssertEqual(
+            BigUInt(1),
+            BigUInt("1000000000")!.toEther(from: .gwei)
+        )
+        XCTAssertEqual(
+            BigUInt(0),
+            BigUInt("10000")!.toEther(from: .gwei)
+        )
+
+        XCTAssertEqual(
+            BigUInt(1),
+            BigUInt(1).toEther(from: .ether)
+        )
+    }
+
+    func testToQuota() {
+        XCTAssertEqual(
+            BigUInt("1000000000000000000"),
+            BigUInt(1).toQuota()
+        )
+    }
+
+    func testFromQuota() {
+        XCTAssertEqual(
+            BigUInt(1),
+            BigUInt("1000000000000000000")!.fromQuota()
+        )
+        XCTAssertEqual(
+            BigUInt(0),
+            BigUInt("1000000000")!.fromQuota()
+        )
+    }
+}
