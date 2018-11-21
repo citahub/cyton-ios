3dffc8713c918c63a56a87063bbd30bbe5d5ea07
cezres <cezr@sina.com>
2018-11-21 22:40:39 +0800

    Remove debug log

--- Neuron/Sections/Wallet/Assets/ManageAssetViewController.swift
+++ Neuron/Sections/Wallet/Assets/ManageAssetViewController.swift
@@ -17,13 +17,11 @@ class ManageAssetViewController: UITableViewController, AssetTableViewCellDelega
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         didGetDataForList()
-        print(CACurrentMediaTime())
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "ERC20列表"
-        print(CACurrentMediaTime())
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
