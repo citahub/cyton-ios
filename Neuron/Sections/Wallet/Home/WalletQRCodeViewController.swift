03e59c5edebcc05ec85144c9ecc07d54400d397f
James Chen <james@ashchan.com>
2018-11-22 09:57:52 +0900

    Delete template comment

--- Neuron/Sections/Wallet/Home/WalletQRCodeViewController.swift
+++ Neuron/Sections/Wallet/Home/WalletQRCodeViewController.swift
@@ -14,13 +14,16 @@ class WalletQRCodeViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var qrCodeView: UIImageView!
     @IBOutlet weak var addressLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         let appModel = WalletRealmTool.getCurrentAppModel()
         iconView.image = UIImage(data: appModel.currentWallet!.iconData!)
         nameLabel.text = appModel.currentWallet!.name
         addressLabel.text = appModel.currentWallet!.address
         let walletAddress = appModel.currentWallet!.address
+
         DispatchQueue.global().async {
             let imagea = EFQRCode.generate(content: walletAddress)
             DispatchQueue.main.async {
@@ -33,14 +36,4 @@ class WalletQRCodeViewController: UIViewController {
         UIPasteboard.general.string = WalletRealmTool.getCurrentAppModel().currentWallet?.address
         Toast.showToast(text: "地址已经复制到粘贴板")
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
