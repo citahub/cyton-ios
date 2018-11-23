fc90e8699eebb64694e2f45fdc21be4a36ed1cbd
James Chen <james@ashchan.com>
2018-11-23 10:17:59 +0900

    Delete textSize func

--- Neuron/Utils/ColorUtils.swift
+++ Neuron/Utils/ColorUtils.swift
@@ -12,7 +12,7 @@ import UIKit
  * 16进制颜色
  * author:xiaolu
  */
-public func ColorFromString (hex: String) -> UIColor {
+public func ColorFromString(hex: String) -> UIColor {
     var cString: String = hex.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
     if cString.hasPrefix("#") {
         cString = (cString as NSString).substring(from: 1)
@@ -25,15 +25,4 @@ public func ColorFromString (hex: String) -> UIColor {
     Scanner(string: gString).scanHexInt32(&g)
     Scanner(string: bString).scanHexInt32(&b)
     return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
-}
-
-/// 计算lable的高度和宽度方法
-///
-/// - Parameters:
-///   - text: text description
-///   - font: font description
-///   - maxSize: maxSize description
-/// - Returns: return value description
-public func textSize(text: String, font: UIFont, maxSize: CGSize) -> CGSize {
-    return text.boundingRect(with: maxSize, options: [.usesLineFragmentOrigin], attributes: [NSAttributedString.Key.font: font], context: nil).size
 }
