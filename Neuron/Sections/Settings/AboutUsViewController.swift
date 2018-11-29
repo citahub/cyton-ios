9381390d7b7ec8f8f8aff5371e815bf4bbc3c36c
LuFP <lfp@cryptape.com>
2018-11-29 20:09:48 +0800

    Update app version to V 0.7.0

--- Neuron/Sections/Settings/AboutUsViewController.swift
+++ Neuron/Sections/Settings/AboutUsViewController.swift
@@ -33,23 +33,9 @@ class AboutUsViewController: UITableViewController {
 
     func setVersionLabel() {
         let infoDictionary = Bundle.main.infoDictionary!
-        let appDisplayName = infoDictionary["CFBundleDisplayName"]
         let majorVersion = infoDictionary["CFBundleShortVersionString"]
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyMMdd"
-        let convertedDate = dateFormatter.string(from: compileDate)
-        versionLabel.text = appDisplayName as? String
-        versionLabel.text = "V \(String(describing: majorVersion!))" + ".\(convertedDate)"
-    }
-
-    var compileDate: Date {
-        let bundleName = Bundle.main.infoDictionary!["CFBundleName"] as? String ?? "Info.plist"
-        if let infoPath = Bundle.main.path(forResource: bundleName, ofType: nil),
-            let infoAttr = try? FileManager.default.attributesOfItem(atPath: infoPath),
-            let infoDate = infoAttr[FileAttributeKey.creationDate] as? Date {
-            return infoDate
-        }
-        return Date()
+        let appBuild = infoDictionary["CFBundleVersion"]
+        versionLabel.text = "V \(String(describing: majorVersion!)).\(String(describing: appBuild!))"
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
