d84424b5ee46e6013bb8c7228ee8333076498ee1
cezres <cezr@sina.com>
2018-12-06 18:43:42 +0800

    Fix create wallet issue

--- Neuron/Services/Common/DefaultTokenAndChain.swift
+++ Neuron/Services/Common/DefaultTokenAndChain.swift
@@ -11,12 +11,18 @@ import RealmSwift
 
 class DefaultTokenAndChain {
     func addDefaultTokenToWallet(wallet: WalletModel) {
-        ethereum(wallet: wallet)
-        mba(wallet: wallet)
+        let walletRef = ThreadSafeReference(to: wallet)
+        DispatchQueue.global().async {
+            let realm = try! Realm()
+            guard let walletModel = realm.resolve(walletRef) else {
+                return
+            }
+            self.ethereum(wallet: walletModel)
+            self.mba(wallet: walletModel)
+        }
     }
 
     func ethereum(wallet: WalletModel) {
-        let walletRef = ThreadSafeReference(to: wallet)
         let ethModel = TokenModel()
         ethModel.address = ""
         ethModel.decimals = NativeDecimals.nativeTokenDecimals
@@ -29,64 +35,55 @@ class DefaultTokenAndChain {
         }
 
         let realm = try! Realm()
-        guard let walletModel = realm.resolve(walletRef) else {
-            return
-        }
         try? realm.write {
             realm.add(ethModel, update: true)
-            if !walletModel.tokenModelList.contains(where: { $0 == ethModel }) {
-                walletModel.tokenModelList.append(ethModel)
+            if !wallet.tokenModelList.contains(where: { $0 == ethModel }) {
+                wallet.tokenModelList.append(ethModel)
             }
-            if !walletModel.selectedTokenList.contains(where: { $0 == ethModel }) {
-                walletModel.selectedTokenList.append(ethModel)
+            if !wallet.selectedTokenList.contains(where: { $0 == ethModel }) {
+                wallet.selectedTokenList.append(ethModel)
             }
         }
     }
 
     func mba(wallet: WalletModel) {
-        let walletRef = ThreadSafeReference(to: wallet)
-        DispatchQueue.global().async {
-            let mbaHost = "http://testnet.mba.cmbchina.biz:1337"
-            do {
-                let metaData = try AppChainNetwork.appChain(url: URL(string: mbaHost)!).rpc.getMetaData()
-                let tokenModel = TokenModel()
-                tokenModel.symbol = metaData.tokenSymbol
-                tokenModel.iconUrl = metaData.tokenAvatar
-                tokenModel.name = metaData.tokenName
-                tokenModel.isNativeToken = true
-                if let tokenIdentifier = TokenModel.identifier(for: tokenModel) {
-                    tokenModel.identifier = tokenIdentifier
-                }
-
-                let chainModel = ChainModel()
-                chainModel.chainId = metaData.chainId
-                chainModel.chainName = metaData.chainName
-                chainModel.httpProvider = mbaHost
-                chainModel.tokenIdentifier = tokenModel.identifier
-                if let chainIdentifier = ChainModel.identifier(for: chainModel) {
-                    chainModel.identifier = chainIdentifier
-                }
-                tokenModel.chain = chainModel
-
-                let realm = try Realm()
-                guard let walletModel = realm.resolve(walletRef) else {
-                    return
-                }
-                try realm.write {
-                    realm.add(tokenModel, update: true)
-                    realm.add(chainModel, update: true)
-                    if !walletModel.selectedTokenList.contains(where: { $0 == tokenModel }) {
-                        walletModel.selectedTokenList.append(tokenModel)
-                    }
-                    if !walletModel.tokenModelList.contains(where: { $0 == tokenModel }) {
-                        walletModel.tokenModelList.append(tokenModel)
-                    }
-                    if !walletModel.chainModelList.contains(where: { $0 == chainModel }) {
-                        walletModel.chainModelList.append(chainModel)
-                    }
-                }
-            } catch {
+        let mbaHost = "http://testnet.mba.cmbchina.biz:1337"
+        do {
+            let metaData = try AppChainNetwork.appChain(url: URL(string: mbaHost)!).rpc.getMetaData()
+            let tokenModel = TokenModel()
+            tokenModel.symbol = metaData.tokenSymbol
+            tokenModel.iconUrl = metaData.tokenAvatar
+            tokenModel.name = metaData.tokenName
+            tokenModel.isNativeToken = true
+            if let tokenIdentifier = TokenModel.identifier(for: tokenModel) {
+                tokenModel.identifier = tokenIdentifier
             }
+
+            let chainModel = ChainModel()
+            chainModel.chainId = metaData.chainId
+            chainModel.chainName = metaData.chainName
+            chainModel.httpProvider = mbaHost
+            chainModel.tokenIdentifier = tokenModel.identifier
+            if let chainIdentifier = ChainModel.identifier(for: chainModel) {
+                chainModel.identifier = chainIdentifier
+            }
+            tokenModel.chain = chainModel
+
+            let realm = try Realm()
+            try realm.write {
+                realm.add(tokenModel, update: true)
+                realm.add(chainModel, update: true)
+                if !wallet.selectedTokenList.contains(where: { $0 == tokenModel }) {
+                    wallet.selectedTokenList.append(tokenModel)
+                }
+                if !wallet.tokenModelList.contains(where: { $0 == tokenModel }) {
+                    wallet.tokenModelList.append(tokenModel)
+                }
+                if !wallet.chainModelList.contains(where: { $0 == chainModel }) {
+                    wallet.chainModelList.append(chainModel)
+                }
+            }
+        } catch {
         }
 
     }
