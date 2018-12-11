50e17ef7f549b507c34696227777aed60c4298d6
LuFP <lfp@cryptape.com>
2018-12-11 18:21:45 +0800

    Complete import ERC20

--- Neuron/Sections/Wallet/Assets/AddAssetController.swift
+++ Neuron/Sections/Wallet/Assets/AddAssetController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 import RealmSwift
+import BLTNBoard
 
 class AddAssetController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var tokenArray: [TokenModel] = []
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var rightBarButton: UIBarButtonItem!
+    private lazy var showTokenPageItem: ShowTokenPageItem = {
+        return ShowTokenPageItem.create()
+    }()
+    private lazy var bulletinManager: BLTNItemManager = {
+        return BLTNItemManager(rootItem: showTokenPageItem)
+    }()
+
     var chain = Chain()
     var inputText = ""
 
@@ -48,6 +56,51 @@ class AddAssetController: UIViewController, UITableViewDelegate, UITableViewData
             appchainNativeToken(nodeAddress: inputText)
         } else {
             appchainERC20Token(chain: chain, contractAddress: inputText)
+        }
+    }
+
+    func showTokenMessage(_ chainModel: ChainModel?, tokenModel: TokenModel) {
+        showTokenPageItem.actionHandler = { item in
+            item.manager?.displayActivityIndicator()
+            self.save(tokenModel: tokenModel, chainModel: chainModel)
+        }
+        showTokenPageItem.update(tokenModel: tokenModel)
+        bulletinManager.showBulletin(above: self)
+    }
+
+    func save(tokenModel: TokenModel, chainModel: ChainModel?) {
+        if TokenModel.identifier(for: tokenModel) != nil {
+            bulletinManager.dismissBulletin()
+            Toast.showToast(text: "Assets.AddAssets.AlreadyExist".localized())
+        }
+        do {
+            let wallet = AppModel.current.currentWallet!
+            let realm = try Realm()
+            try realm.write {
+                realm.add(tokenModel, update: true)
+                if !wallet.selectedTokenList.contains(where: { $0 == tokenModel }) {
+                    wallet.selectedTokenList.append(tokenModel)
+                }
+                if !wallet.tokenModelList.contains(where: { $0 == tokenModel }) {
+                    wallet.tokenModelList.append(tokenModel)
+                }
+                if chainModel != nil {
+                    realm.add(chainModel!, update: true)
+                    if !wallet.chainModelList.contains(where: { $0 == chainModel }) {
+                        wallet.chainModelList.append(chainModel!)
+                    }
+                }
+            }
+            let successPageItem = SuccessPageItem.create(title: "DApp.Contract.TransactionSend".localized())
+            successPageItem.actionHandler = { item in
+                self.bulletinManager.dismissBulletin()
+                Toast.showToast(text: "Assets.AddAssets.StoreSuccess".localized())
+                self.navigationController?.popViewController(animated: true)
+            }
+            bulletinManager.push(item: successPageItem)
+        } catch {
+            bulletinManager.dismissBulletin()
+            Toast.showToast(text: "Assets.AddAssets.StoreFailed".localized())
         }
     }
 
@@ -93,7 +146,7 @@ class AddAssetController: UIViewController, UITableViewDelegate, UITableViewData
             DispatchQueue.main.async {
                 Toast.hideHUD()
                 if result != nil {
-
+                    self.showTokenMessage(nil, tokenModel: result!)
                 } else {
                     Toast.showToast(text: "Assets.AddAssets.EmptyResult".localized())
                 }
@@ -109,7 +162,7 @@ class AddAssetController: UIViewController, UITableViewDelegate, UITableViewData
             DispatchQueue.main.async {
                 Toast.hideHUD()
                 if tokenModel != nil && chainModel != nil {
-
+                    self.showTokenMessage(chainModel!, tokenModel: tokenModel!)
                 } else {
                     Toast.showToast(text: "Assets.AddAssets.EmptyResult".localized())
                 }
@@ -124,7 +177,7 @@ class AddAssetController: UIViewController, UITableViewDelegate, UITableViewData
             DispatchQueue.main.async {
                 Toast.hideHUD()
                 if tokenModel != nil {
-
+                    self.showTokenMessage(nil, tokenModel: tokenModel!)
                 } else {
                     Toast.showToast(text: "Assets.AddAssets.EmptyResult".localized())
                 }
